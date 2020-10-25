ca2f324bff18e2a9ac45a2610358ea82a4f2f89e
tseringp2 <pelmo@wisc.edu>
2020-10-24 23:23:37 -0500

    summary

---
+++ Tsering-Comment Folder/commentsOutline.py
@@ -0,0 +1,19 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+#hi
+
+
+#2) Make a multi-line comment.
+'''
+hi
+'''
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+var = 6 #hello
+
