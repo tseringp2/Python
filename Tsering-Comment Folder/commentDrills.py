4dfc2a71f9bfb048e8fc7e25c8d9062705a3e98f
tseringp2 <pelmo@wisc.edu>
2020-10-25 23:04:20 -0500

    summary

---
+++ Tsering-Comment Folder/commentDrills.py
@@ -0,0 +1,40 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+
+#hi
+
+#2) Make a multi-line comment.
+
+'''
+hello there
+'''
+
+#3) Make a variable. Then put a comment after it, on the same line.
+
+var = (2) #this is two
+
+#4) Make a single line comment.
+
+#another comment
+
+#5) Make a multi-line comment.
+
+'''
+another comment
+'''
