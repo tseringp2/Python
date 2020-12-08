dbb62d1833c83c2f8b07f42ec46ed4af4c63b5de
tseringp2 <pelmo@wisc.edu>
2020-12-08 00:45:07 -0600

    summary

---
+++ Mathematical Operators/mathOutline.py
@@ -0,0 +1,84 @@
+'''
+This outline will help solidify concepts from the Mathematical Operators lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make two string variables and use the + operator to combine them into
+#one new variable.
+var = "hello"
+varTwo = "there"
+y = var + varTwo
+
+#2) Make two int variables and use the + operator to combine them into
+#one new variable.
+var = 1
+varTwo = 2
+y = var + varTwo 
+
+
+#3) Make two int variables and use the - operator to combine them into
+#one new variable.
+var = 5
+varTwo = 1
+y = var - varTwo
+
+
+#4) Make two float variables and use the - operator to combine them into
+#one new variable.
+var = 3.5
+varTwo = 2.2
+y = var - varTwo
+
+#5) Make two int variables and use the * operator to combine them into
+#one new variable.
+var = 2
+varTwo = 3
+y = var * varTwo
+
+
+#6) Make one string variable and one int variable and use the * operator 
+#to combine them into one new variable.
+var = "good"
+varTwo = 2
+y= var * varTwo
+
+
+#7) Make two int variables and use the / operator to combine them into
+#one new variable.
+var = 8
+varTwo = 4
+y = var / varTwo
+
+#8) Make two float variables and use the / operator to combine them into
+#one new variable.
+var = 4.5
+varTwo = 2.5
+y = var / varTwo
+
+
+#9) Make two int variables and use the % operator to combine them into
+#one new variable.
+var = 6
+varTwo = 2
+y = var % varTwo
+
+
+#10) Make two int variables and use the // operator to combine them into
+#one new variable.
+var = 4
+varTwo = 2
+y = var // varTwo
+
+
+#11) Make two int variables and use the ** operator to combine them into
+#one new variable.
+var = 5
+varTwo = 2
+y = var ** varTwo
+
+
+#12) Make any type of operation with a common SYNTAX error
+var = 4
+varTwo = 3
+y = var + narTwo
+
