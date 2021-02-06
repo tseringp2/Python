a7601fe3d9096d140dbbc5982f7c0a71b5677c92
tseringp2 <pelmo@wisc.edu>
2021-02-06 00:13:54 -0600

    summary

---
+++ Conversion/Conversion.py
@@ -0,0 +1,35 @@
+'''
+Make a program that can complete different conversions.
+
+@author: Tsering Pelmo
+'''
+#Use input() to get the number of miles from the user. And store
+#that int in a variable called miles.
+
+#Convert miles to yards, using the following:
+# 1 mile = 1760 yards.
+miles = 1
+yards = 1760
+sentence = (str(miles) + " miles converts to " + str(yards) + " yards. ")
+#Store the value in a variable called yards and print it out with a 
+#simple statement.
+yards = sentence
+print(yards)
+#Convert miles to feet, using the following:
+# 1 mile = 5280 feet.
+miles = 1
+feet = 5280
+sentence = (str(miles) + " miles converts to " + str(feet) + " feet. ")
+#Store the value in a variable called feet and print it out with a 
+#simple statement.
+feet = sentence
+print(feet)
+#Convert miles to inches, using the following:
+# 1 mile = 63,360 inches.
+miles = 1
+inches = 63360
+sentence = (str(miles) + " miles converts to " + str(inches) + " inches. ")
+#Store the value in a variable called inches and print it out with a 
+#simple statement.
+inches = sentence
+print(inches)
